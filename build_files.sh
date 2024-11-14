e5946ab7c5197e9a972b8394d9de0dd884eb820f
Anurag Maurya <myanurag0202@gmail.com>
2024-11-14 12:18:02 +0530

    fixing build...

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Building projects package.."
+echo "Ensuring pip is installed..."
+python3.9 -m ensurepip --upgrade
+
+# Verify that pip was installed
+python3.9 -m pip --version || (echo "pip failed to install"; exit 1)
+
+echo "Upgrading pip..."
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
