e8358778d901cbb570ca73248b5bf27f8d9feb6d
Anurag Maurya <myanurag0202@gmail.com>
2024-11-14 11:51:46 +0530

    modified vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/bin/bash
+
+echo "Building projects package.."
+python3.10 pip install -r requirements.txt
+
+echo "migrating database..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
