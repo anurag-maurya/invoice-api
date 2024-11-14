966214d3610fb5dbf25ca7e56d5e3ee8f7a7cbe0
Anurag Maurya <myanurag0202@gmail.com>
2024-11-14 12:07:45 +0530

    fixed deployment

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Building projects package.."
-python3.10 pip install -r requirements.txt
+python pip install -r requirements.txt
 
 echo "migrating database..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
