44127c143fd8eb3d7435388cb117e4ca2474edac
Anurag Maurya <myanurag0202@gmail.com>
2024-11-14 12:52:54 +0530

    fixing build....

--- payment/settings.py
+++ payment/settings.py
@@ -17,9 +17,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_file = os.path.join(BASE_DIR, '.env')
+
 env = environ.Env()
-environ.Env.read_env(env_file)
+
+if not env.str('DB_HOST', default=None):
+    env_file = os.path.join(BASE_DIR, '.env')
+    environ.Env.read_env(env_file)  # This will load from .env if exists locally
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
